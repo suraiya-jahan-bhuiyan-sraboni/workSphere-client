d7c9cbb44dd723dd51bfb7dfb1a81a3a2d0f4e88
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-07-24 05:37:55 +0600

    footer done

--- src/root/root components/Footer.jsx
+++ src/root/root components/Footer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-2 border-t ">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        {/* Company Info */}
+        <div className="mb-4 md:mb-0">
+          <h3 className="text-xl font-bold">Workey</h3>
 
-export default Footer
+        </div>
+        <p className="text-xs mb-3 md:mb-0">
+          &copy; {new Date().getFullYear()} Workey. All rights reserved.
+        </p>
+        {/* Navigation Links */}
+        <ul className="flex space-x-4 ">
+          <li>
+            <a href="#" className="hover:underline text-sm">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline text-sm">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline text-sm">
+              Privacy Policy
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
